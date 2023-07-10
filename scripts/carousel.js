0497be57a6a931f7735a9df37e2a44b7d664d377
NimmBle <icancube96@gmail.com>
2023-07-10 14:16:51 +0300

    finished effects

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,13 +1,31 @@
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("arrow-prev");
-const nextButton = document.getElementById("arrow-next");
+const slidesContainerHeader = document.getElementById("slides-container-header");
+const slideHeader = document.querySelector(".slide-header");
+const prevButtonHeader = document.getElementById("arrow-prev-header");
+const nextButtonHeader = document.getElementById("arrow-next-header");
 
-nextButton.addEventListener("click", (event) => {
-    const slideWidth = slide.clientWidth;
-      slidesContainer.scrollLeft += slideWidth;
-  });
-prevButton.addEventListener("click", () => {
-    const slideWidth = slide.clientWidth;
-    slidesContainer.scrollLeft -= slideWidth;
+prevButtonHeader.addEventListener("click", () => {
+  const slideWidth = slideHeader.clientWidth;
+  slidesContainerHeader.scrollLeft -= slideWidth;
+});
+
+nextButtonHeader.addEventListener("click", (event) => {
+  console.log(nextButtonHeader)
+  const slideWidth = slideHeader.clientWidth;
+  slidesContainerHeader.scrollLeft += slideWidth;
+});
+
+
+const slidesContainerQuotes = document.getElementById("slides-container-quotes");
+const slideQuotes = document.querySelector(".slide-quotes");
+const prevButtonQuotes = document.getElementById("arrow-prev-quotes");
+const nextButtonQuotes = document.getElementById("arrow-next-quotes");
+
+prevButtonQuotes.addEventListener("click", () => {
+  const slideWidth = slideQuotes.clientWidth;
+  slidesContainerQuotes.scrollLeft -= slideWidth;
+});
+nextButtonQuotes.addEventListener("click", (event) => {
+  console.log(nextButtonQuotes)
+  const slideWidth = slideQuotes.clientWidth;
+  slidesContainerQuotes.scrollLeft += slideWidth;
 });
