c362179a101a3e3b019ae4617c5c6afc12b29913
Hristo Panev <icancube96@gmail.com>
2023-07-13 00:16:53 +0300

    mobile responsive - completed

---
+++ scripts/hamburger.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger')
+const navlinks = document.querySelector('.nav__links')
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    hamburger.classList.toggle()
+} )
