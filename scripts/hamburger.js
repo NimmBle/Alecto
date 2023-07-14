f022f3614f362e05817b00607cb2b44739ae5573
Hristo Panev <icancube96@gmail.com>
2023-07-14 16:29:24 +0300

    kind of done

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,5 +2,5 @@ const hamburger = document.querySelector('.hamburger')
 const navlinks = document.querySelector('.nav__links')
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
-    hamburger.classList.toggle()
+    navlinks.classList.toggle('hidden')
 } )
